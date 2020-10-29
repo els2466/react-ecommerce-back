b2626d8d267b4f84d1f82cea2efdc35cfd3a4412
Ed Sterling <els2466@gmail.com>
2020-10-29 11:50:45 -0500

    first commit

---
+++ firebase/index.js
@@ -0,0 +1,10 @@
+var admin = require("firebase-admin");
+
+var serviceAccount = require("../config/fbServiceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://ecommerce-6db43.firebaseio.com"
+});
+
+module.exports = admin;
